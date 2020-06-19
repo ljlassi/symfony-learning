0503c6310d2f7664404f14f9bf6776525cb536b9
Lassi Jääskeläinen <lassijaa@gmail.com>
2020-06-19 20:00:18 +0300

    Added template for adding product (form), updated home template with a link to the adding product template. Worked on ProductController, added functionality to render the add product view, and worked on createProduct() so that it actually works and saves to database based on POST request

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,33 +4,49 @@
 namespace App\Controller;
 
 use App\Entity\Product;
-use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 
 class ProductController
 {
 
     /**
-     * @Route("/product", name="create_product")
+     * @Route("/create/product", name="create_product")
      *
-     * @param string
-     * @param int
      *
      * @return String
      */
 
-    public function createProduct(string $name, int $price) : Response {
+    public function createProduct(EntityManagerInterface $entityManager) : Response {
 
-        $entityManager = $this->getDoctrine()->getManager();
+        $request = Request::createFromGlobals();
+        $product_name = $request->request->get('product_name');
+        $product_price = $request->request->get('product_price');
+        
         $product = new Product();
-        $product->setName($name);
-        $product->setPrice($price);
+        $product->setName($product_name);
+        $product->setPrice($product_price);
 
         $entityManager->persist($product);
         $entityManager->flush();
 
+        return new Response('Saved new product named: ' . $product->getName());
 
+
+    }
+
+    /**
+     * @Route("/new/product", name="new_product")
+     *
+     * @return String
+     */
+
+    public function createProductForm(Environment $twig) : Response {
+        return new Response($twig->render("/add_product.html.twig", []));
     }
 
 }
