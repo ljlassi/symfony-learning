848b266949dcc0ccc56af2f3f5c716db8c560630
Lassi Jääskeläinen <lassijaa@gmail.com>
2020-07-02 11:50:47 +0300

    Worked on response tests for ProductController, adding a product in test environment still fails, working on it

--- tests/Controller/ProductControllerTest.php
+++ tests/Controller/ProductControllerTest.php
@@ -18,4 +18,25 @@ class ProductControllerTest extends WebTestCase
         $this->assertEquals(200, $response);
     }
 
+    /**
+     * THIS TEST FAILS, gives null for some reason, I'm working on it.
+     */
+
+    public function testCreateProduct() : void {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/new/product');
+
+        //$crawler = $client->submitForm('add_product_submit', ['product_name' => 'test_product', 'product_price' => 100]);
+
+        $form = $crawler->selectButton('add_product_submit')->form();
+        $form['product_name'] = 'test_product';
+        $form['product_price'] = '100';
+
+// submit that form
+        $crawler = $client->submit($form);
+
+        $response = $client->getResponse()->getStatusCode();
+        $this->assertEquals(200, $response);
+    }
+
 }
